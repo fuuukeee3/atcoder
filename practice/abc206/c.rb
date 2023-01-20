771172739d98018332c09e01bd6182ffa261d065
fu <semarusika@gmail.com>
2023-01-20 22:46:55 +0900

    deer

---
+++ practice/abc206/c.rb
@@ -0,0 +1,13 @@
+require "set"
+n = gets.chomp.to_i
+arr = gets.chomp.split(" ").map(&:to_i)
+
+hash = Hash.new { 0 }
+count = 0
+(0...n).each do |i|
+  count += i - hash[arr[i]]
+  p [i, hash[arr[i]], count]
+  hash[arr[i]] += 1
+end
+
+puts count
