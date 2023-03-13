b73f1de193bdbb3a7a7b5c439e1f9b7b17f9301d
fu <semarusika@gmail.com>
2023-03-14 07:59:10 +0900

    abc214

--- practice/abc214/c.rb
+++ practice/abc214/c.rb
@@ -2,23 +2,9 @@ n = gets.chomp.to_i
 sunuke = gets.chomp.split(" ").map(&:to_i)
 takahashi = gets.chomp.split(" ").map(&:to_i)
 
-current = takahashi.find_index(takahashi.min)
-results = []
-results[current] = takahashi[current]
-
-(n-1).times do
-  value1 = results[current] + sunuke[current]
-  target = current + 1
-
-  if target >= n
-    target -= n
-  end
-  value2 = takahashi[target]
-
-  results[target] = value
-  current += 1
+results = takahashi.dup
+(n * 2).times do |i|
+  results[(i + 1) % n] = [results[(i + 1) % n], results[i % n] + sunuke[i % n]].min
 end
 
-p results
-
-
+puts results.join("\n")
