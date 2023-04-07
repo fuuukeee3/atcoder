1430bac0be5829b0b2561e5f65faa36784b58c2c
fu <semarusika@gmail.com>
2023-04-07 22:11:57 +0900

    stack

--- argo_and_data/part4/alds1_3_a.go
+++ argo_and_data/part4/alds1_3_a.go
@@ -1,49 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	fmt.Println(stack("1 2 + 3 4 - *"))
+type Stack struct {
+	data []int
 }
 
-func stack(s string) int {
-	stack := []int{}
+func (s *Stack) push(a int) {
+	s.data = append(s.data, a)
+}
+func (s *Stack) pop() int {
+	rtn := s.data[len(s.data)-1]
+	s.data = s.data[0 : len(s.data)-1]
+	return rtn
+}
 
-	//rex := regexp.MustCompile("[0-9]]+")
+func main() {
+	//fmt.Println(poland("1 2 + 3 4 - *"))
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	fmt.Println(poland(scanner.Text()))
+}
+
+func poland(s string) int {
+	stack := Stack{}
+
 	for _, ss := range strings.Split(s, " ") {
 		if ss == "+" {
-			b := stack[len(stack)-1]
-			stack = stack[0 : len(stack)-1]
-
-			a := stack[len(stack)-1]
-			stack = stack[0 : len(stack)-1]
-
-			stack = append(stack, a+b)
+			b := stack.pop()
+			a := stack.pop()
+			stack.push(a + b)
 		} else if ss == "-" {
-			b := stack[len(stack)-1]
-			stack = stack[0 : len(stack)-1]
-
-			a := stack[len(stack)-1]
-			stack = stack[0 : len(stack)-1]
-
-			stack = append(stack, a-b)
+			b := stack.pop()
+			a := stack.pop()
+			stack.push(a - b)
 		} else if ss == "*" {
-			b := stack[len(stack)-1]
-			stack = stack[0 : len(stack)-1]
-
-			a := stack[len(stack)-1]
-			stack = stack[0 : len(stack)-1]
-
-			stack = append(stack, a*b)
+			b := stack.pop()
+			a := stack.pop()
+			stack.push(a * b)
 		} else {
 			a, _ := strconv.Atoi(ss)
-			stack = append(stack, a)
+			stack.push(a)
 		}
 	}
 
-	return stack[0]
+	return stack.data[0]
 }
