8175aa6978cfca9de5a7469c2387d959bdebd8bd
fu <semarusika@gmail.com>
2023-05-09 08:37:58 +0900

    practice

--- practice/abc100/b.rb
+++ practice/abc100/b.rb
@@ -1,22 +1,21 @@
 d, n = gets.chomp.split(" ").map(&:to_i)
-results = []
 case d
 when 0
-  (1..1000).each do |i|
-    results << i if i % 100 != 0
+  if n == 100
+    puts n + 1
+  else
+    puts n
   end
 when 1
-  (1..1000).each do |i|
-    ii = i * 100
-    div, mod = ii.divmod(100)
-    results << ii if div >= 1 && mod == 0
+  if n ==  100
+    puts n * 100 + 1 * 100
+  else
+    puts n * 100
   end
 when 2
-  (1..1000).each do |i|
-    ii = i * 100 * 100
-    div, mod = ii.divmod(100 * 100)
-    results << ii if div >= 1 && mod == 0
+  if n == 100
+    puts n * 100 * 100 + 1 * 100 * 100
+  else
+    puts n * 100 * 100
   end
 end
-
-puts results[n - 1]
