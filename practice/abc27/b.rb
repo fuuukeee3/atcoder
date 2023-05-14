4897dc3ee230742488670ee273ebc81cab3d6cd9
fu <semarusika@gmail.com>
2023-05-15 06:55:40 +0900

    practice

--- practice/abc27/b.rb
+++ practice/abc27/b.rb
@@ -1,10 +1,24 @@
 n = gets.chomp.to_i
 a = gets.chomp.split(" ").map(&:to_i)
 
-people, mod = a.sum.divmod(n)
+asum = a.sum
+people, mod = asum.divmod(n)
 
 unless mod.zero?
   puts -1
   exit
 end
 
+count = 0
+(0...(n - 1)).each do |i|
+  left = a[0..i].sum
+  right = a[i + 1..-1].sum
+
+  left2 = people * (i + 1)
+  right2 = asum - left2
+
+  next if left == left2 && right == right2
+  count += 1
+end
+
+puts count
