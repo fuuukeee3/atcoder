3aa1198420f2fbfc7aae3549760172b901252271
fu <semarusika@gmail.com>
2023-05-18 06:57:38 +0900

    practice

---
+++ practice/abc30/b.rb
@@ -0,0 +1,8 @@
+n, m = gets.chomp.split(" ").map(&:to_i)
+short_move = 360.0 / 60
+long_move = 360.0 / 12 / 60
+
+short_position = short_move * (n-12)
+long_position =
+p [short_move, short_position]
+p [long_move]
--- practice/abc54/b.rb
+++ practice/abc54/b.rb
@@ -9,13 +9,43 @@ m.times do
   b << gets.chomp.split("")
 end
 
-# p a
-# p b
+template = b.map { |bb| bb.join("") }.join("")
+flg = false
 
 # 縦移動
-(0..(n-m)).each do |h|
-  (0..(n-m)).each do |w|
-  p [h,w]
-  # p a[h][h...h+m]
+(0..(n - m)).each do |h|
+  (0..(n - m)).each do |w|
+    # tmp = Array.new(m) { Array.new(m) }
+    str = ""
+    m.times do |i|
+      m.times do |j|
+        # tmp[i][j] = a[w + i][h + j]
+        str += a[w + i][h + j]
+      end
+    end
+
+    if template == str
+      flg = true
+    end
   end
 end
+
+# 横移動
+(0..(n - m)).each do |h|
+  (0..(n - m)).each do |w|
+    # tmp = Array.new(m) { Array.new(m) }
+    str = ""
+    m.times do |i|
+      m.times do |j|
+        # tmp[i][j] = a[w + i][h + j]
+        str += a[h + i][w + j]
+      end
+    end
+
+    if template == str
+      flg = true
+    end
+  end
+end
+
+puts flg ? "Yes" : "No"
