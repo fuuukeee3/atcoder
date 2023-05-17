ffe4701aedc0573ee984824277f27d1d0ff1c711
fu <semarusika@gmail.com>
2023-05-17 23:44:08 +0900

    practice

--- practice/abc255/b.rb
+++ practice/abc255/b.rb
@@ -1,8 +1,33 @@
 n, k = gets.chomp.split(" ").map(&:to_i)
 arr = gets.chomp.split(" ").map(&:to_i)
-xy = []
+peoples = []
 n.times do
   x, y = gets.chomp.split(" ").map(&:to_i)
-  xy << [x, y]
+  peoples << [x, y]
 end
 
+max = 0
+peoples.each do |people|
+  pmin = 99999999999
+  arr.each do |i|
+    x = peoples[i-1][0]
+    y = peoples[i-1][1]
+
+    if people[0] == x && people[1] == y
+      pmin = [pmin, 0].min
+    elsif people[0] == x
+      pmin = [pmin, (people[1] - y).abs].min
+    elsif people[1] == y
+      pmin = [pmin, (people[0] - x).abs].min
+    else
+      xx = (people[0] - x).abs
+      yy = (people[1] - y).abs
+      cc = Math.sqrt(xx ** 2 + yy ** 2)
+      pmin = [pmin, cc].min
+    end
+  end
+
+  max = [max, pmin].max
+end
+
+puts max
