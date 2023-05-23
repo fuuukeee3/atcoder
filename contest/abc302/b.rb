53328640ca3bd94b2d368c93e390b71a2acdd8e8
fu <semarusika@gmail.com>
2023-05-23 21:25:12 +0900

    refactor

--- contest/abc302/b.rb
+++ contest/abc302/b.rb
@@ -1,151 +1,158 @@
+def get_all_directions_for_grid(grid, current, size)
+  hash = {
+    l: [],
+    r: [],
+    u: [],
+    d: [],
+    lu: [],
+    ld: [],
+    ru: [],
+    rd: [],
+  }
+
+  h = grid.size
+  w = grid[0].size
+
+  # 左
+  hash[:l] << grid[current[0]][current[1]]
+  j = current[1] - 1
+  (size - 1).times do
+    break if j < 0
+
+    hash[:l] << grid[current[0]][j]
+    j -= 1
+  end
+
+  # 右
+  hash[:r] << grid[current[0]][current[1]]
+  j = current[1] + 1
+  (size - 1).times do
+    break if j >= w
+
+    hash[:r] << grid[current[0]][j]
+    j += 1
+  end
+
+  # 上
+  hash[:u] << grid[current[0]][current[1]]
+  i = current[0] - 1
+  (size - 1).times do
+    break if i < 0
+
+    hash[:u] << grid[i][current[1]]
+    i -= 1
+  end
+
+  # 下
+  hash[:d] << grid[current[0]][current[1]]
+  i = current[0] + 1
+  (size - 1).times do
+    break if i >= h
+
+    hash[:d] << grid[i][current[1]]
+    i += 1
+  end
+
+  # 左上
+  hash[:lu] << grid[current[0]][current[1]]
+  i = current[0] - 1
+  j = current[1] - 1
+  (size - 1).times do
+    break if i < 0 || j < 0
+
+    hash[:lu] << grid[i][j]
+    i -= 1
+    j -= 1
+  end
+
+  # 左下
+  hash[:ld] << grid[current[0]][current[1]]
+  i = current[0] + 1
+  j = current[1] - 1
+  (size - 1).times do
+    break if i >= h || j < 0
+
+    hash[:ld] << grid[i][j]
+    i += 1
+    j -= 1
+  end
+
+  # 右上
+  hash[:ru] << grid[current[0]][current[1]]
+  i = current[0] - 1
+  j = current[1] + 1
+  (size - 1).times do
+    break if i < 0 || j >= w
+
+    hash[:ru] << grid[i][j]
+    i -= 1
+    j += 1
+  end
+
+  # 右下
+  hash[:rd] << grid[current[0]][current[1]]
+  i = current[0] + 1
+  j = current[1] + 1
+  (size - 1).times do
+    break if i >= h || j >= w
+
+    hash[:rd] << grid[i][j]
+    i += 1
+    j += 1
+  end
+
+  hash
+end
+
 h, w = gets.chomp.split(" ").map(&:to_i)
 s = []
 h.times do
   s << gets.chomp.split("")
 end
 
-# 0:下 1:右 2:上 3:左 4:右下 5:右上 6:左下 7:左上
-dh = [1, 0, -1, 0, 1, -1, -1, 1]
-dw = [0, 1, 0, -1, 1, 1, -1, -1]
-
 h.times do |i|
   w.times do |j|
     next if s[i][j] != "s"
 
-    # 右
-    str = 's'
-    j2 = j + 1
-    4.times do
-      break if j2 >= w
-      str += s[i][j2]
-      j2 += 1
+    hash = get_all_directions_for_grid(s, [i, j], 5)
+    key = nil
+    hash.each do |k, v|
+      key = k if "snuke" == v.join("")
     end
 
-    if str == "snuke"
-      5.times do |i3|
-        puts "#{i + 1} #{j + i3 + 1}"
+    case key
+    when :l
+      5.times do |ii|
+        puts "#{i + 1} #{j - ii + 1}"
       end
-      break
-    end
-
-    # 左
-    str = 's'
-    j2 = j - 1
-    4.times do
-      break if j2 < 0
-      str += s[i][j2]
-      j2 -= 1
-    end
-
-    if str == "snuke"
-      5.times do |i3|
-        puts "#{i + 1} #{j - i3 + 1}"
+    when :r
+      5.times do |ii|
+        puts "#{i + 1} #{j + ii + 1}"
       end
-      break
-    end
-
-    # 上
-    str = 's'
-    i2 = i - 1
-    4.times do
-      break if i2 < 0
-      str += s[i2][j]
-      i2 -= 1
-    end
-
-    if str == "snuke"
-      5.times do |i3|
-        puts "#{i - i3 + 1} #{j + 1}"
+    when :u
+      5.times do |ii|
+        puts "#{i - ii + 1} #{j + 1}"
       end
-      break
-    end
-
-    # 下
-    str = 's'
-    i2 = i + 1
-    4.times do
-      break if i2 >= h
-      str += s[i2][j]
-      i2 += 1
-    end
-
-    if str == "snuke"
-      5.times do |i3|
-        puts "#{i + i3 + 1} #{j + 1}"
+    when :d
+      5.times do |ii|
+        puts "#{i + ii + 1} #{j + 1}"
       end
-      break
-    end
-
-    # 右上
-    str = 's'
-    i2 = i - 1
-    j2 = j + 1
-    4.times do
-      break if i2 < 0 || j2 >= w
-      str += s[i2][j2]
-      i2 -= 1
-      j2 += 1
-    end
-
-    if str == "snuke"
-      5.times do |i3|
-        puts "#{i - i3 + 1} #{j + i3 + 1}"
+    when :lu
+      5.times do |ii|
+        puts "#{i - ii + 1} #{j - ii + 1}"
       end
-      break
-    end
-
-    # 右下
-    str = 's'
-    i2 = i + 1
-    j2 = j + 1
-    4.times do
-      break if i2 >= h || j2 >= w
-      str += s[i2][j2]
-      i2 += 1
-      j2 += 1
-    end
-
-    if str == "snuke"
-      5.times do |i3|
-        puts "#{i + i3 + 1} #{j + i3 + 1}"
+    when :ld
+      5.times do |ii|
+        puts "#{i + ii + 1} #{j - ii + 1}"
       end
-      break
-    end
-
-    # 左上
-    str = 's'
-    i2 = i - 1
-    j2 = j - 1
-    4.times do
-      break if i2 < 0 || j2 < 0
-      str += s[i2][j2]
-      i2 -= 1
-      j2 -= 1
-    end
-
-    if str == "snuke"
-      5.times do |i3|
-        puts "#{i - i3 + 1} #{j - i3 + 1}"
+    when :ru
+      5.times do |ii|
+        puts "#{i - ii + 1} #{j + ii + 1}"
       end
-      break
-    end
-
-    # 左下
-    str = 's'
-    i2 = i + 1
-    j2 = j - 1
-    4.times do
-      break if i2 >= h || j2 < 0
-      str += s[i2][j2]
-      i2 += 1
-      j2 -= 1
-    end
-
-    if str == "snuke"
-      5.times do |i3|
-        puts "#{i + i3 + 1} #{j - i3 + 1}"
+    when :rd
+      5.times do |ii|
+        puts "#{i + ii + 1} #{j + ii + 1}"
       end
-      break
     end
   end
 end
--- util/util.rb
+++ util/util.rb
@@ -88,3 +88,114 @@ end
   end
   a = new_a # 回転済みのGridを回転するので元Gridを上書き
 end
+
+# gridから上下左右斜めの8方向に走査して取得して値を取得する
+# @param [Array<String|Integer>] grid 走査対象grid
+# @param [Array<Integer>] current 現在位置 ex) [0,1]
+# @param [Integer] size 取得サイズ
+# @return [Hash]
+def get_all_directions_for_grid(grid, current, size)
+  hash = {
+    l: [],
+    r: [],
+    u: [],
+    d: [],
+    lu: [],
+    ld: [],
+    ru: [],
+    rd: [],
+  }
+
+  h = grid.size
+  w = grid[0].size
+
+  # 左
+  hash[:l] << grid[current[0]][current[1]]
+  j = current[1] - 1
+  (size - 1).times do
+    break if j < 0
+
+    hash[:l] << grid[current[0]][j]
+    j -= 1
+  end
+
+  # 右
+  hash[:r] << grid[current[0]][current[1]]
+  j = current[1] + 1
+  (size - 1).times do
+    break if j >= w
+
+    hash[:r] << grid[current[0]][j]
+    j += 1
+  end
+
+  # 上
+  hash[:u] << grid[current[0]][current[1]]
+  i = current[0] - 1
+  (size - 1).times do
+    break if i < 0
+
+    hash[:u] << grid[i][current[1]]
+    i -= 1
+  end
+
+  # 下
+  hash[:d] << grid[current[0]][current[1]]
+  i = current[0] + 1
+  (size - 1).times do
+    break if i >= h
+
+    hash[:d] << grid[i][current[1]]
+    i += 1
+  end
+
+  # 左上
+  hash[:lu] << grid[current[0]][current[1]]
+  i = current[0] - 1
+  j = current[1] - 1
+  (size - 1).times do
+    break if i < 0 || j < 0
+
+    hash[:lu] << grid[i][j]
+    i -= 1
+    j -= 1
+  end
+
+  # 左下
+  hash[:ld] << grid[current[0]][current[1]]
+  i = current[0] + 1
+  j = current[1] - 1
+  (size - 1).times do
+    break if i >= h || j < 0
+
+    hash[:ld] << grid[i][j]
+    i += 1
+    j -= 1
+  end
+
+  # 右上
+  hash[:ru] << grid[current[0]][current[1]]
+  i = current[0] - 1
+  j = current[1] + 1
+  (size - 1).times do
+    break if i < 0 || j >= w
+
+    hash[:ru] << grid[i][j]
+    i -= 1
+    j += 1
+  end
+
+  # 右下
+  hash[:rd] << grid[current[0]][current[1]]
+  i = current[0] + 1
+  j = current[1] + 1
+  (size - 1).times do
+    break if i >= h || j >= w
+
+    hash[:rd] << grid[i][j]
+    i += 1
+    j += 1
+  end
+
+  hash
+end
