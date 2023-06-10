faa2a19c2bbdbd7a5cd1158145ac84eb46cd0784
fu <semarusika@gmail.com>
2023-06-10 23:22:38 +0900

    abc305

--- contest/abc305/d.rb
+++ contest/abc305/d.rb
@@ -1,35 +1,27 @@
 n = gets.chomp.to_i
 log = gets.chomp.split(" ").map(&:to_i)
 
-log2 = { 0 => 0 }
-log.each.with_index do |l, index|
-  next if l.zero?
+# A[i]までに何分寝たか求める
+tmp_sleep_time = Array.new(n)
+log.each_with_index do |l, index|
+  if index.zero?
+    tmp_sleep_time[index] = 0
+    next
+  end
 
-  if index.odd?
-    log2[l] = log2[log[index - 1]]
+  if index.even?
+    tmp_sleep_time[index] = tmp_sleep_time[index-1] + l - log[index-1]
   else
-    log2[l] = log2[log[index - 1]] + (l - log[index - 1])
+    tmp_sleep_time[index] = tmp_sleep_time[index-1]
   end
 end
 
-p log2
-keys = log2.keys
-# p keys
+# x分までに何分寝たか
+
 
 q = gets.chomp.to_i
 q.times do
   l, r = gets.chomp.split(" ").map(&:to_i)
-  s = keys.bsearch { |x| x >= l }
-  e = keys.bsearch { |x| x >= r }
-  p [s, e]
-
-  tmp = log2[e] - log2[s]
-  if l < s
-    index = keys.find_index(s) - 1
-    p index
-      tmp -= log2[s] - (s - l)
-  else
-    tmp -= log2[s]
-  end
-  p tmp
 end
+
+p tmp_sleep_time
