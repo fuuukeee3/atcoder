01aeeeb6ad71613f78bb65a8111776a135c494ad
fu <semarusika@gmail.com>
2023-07-24 06:49:17 +0900

    practice

--- practice/abc311/c.rb
+++ practice/abc311/c.rb
@@ -1,26 +1,21 @@
 n = gets.chomp.to_i
 arr = gets.chomp.split(" ").map(&:to_i)
-
-g = Array.new(n + 1)
-arr.each_with_index do |a, index|
-  (g[index + 1] ||= []) << a
-end
-
-
-r = []
-flgs = Hash.new { false }
-a = [1]
-nex = 1
+arr.unshift(0)
+now = 1
 
 n.times do
-  if flgs[g[nex][0]]
-    r << g[nex][0]
-    flgs[g[nex][0]] = false
-  end
-  a << g[nex][0]
-  flgs[a[-1]] = true
-  nex = a[-1]
+  now = arr[now]
 end
 
-p a
-p r
+r = [now]
+loop do
+  if r[0] == arr[now]
+    break
+  end
+  now = arr[now]
+  r << now
+end
+
+puts r.size
+puts r.join(" ")
+
